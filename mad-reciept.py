12b245123ec67a3cf035fa1e528c63f539eaa017
vatsal-haresh-shah <67568173+vatsal-haresh-shah@users.noreply.github.com>
2021-05-14 12:03:38 +0530

    Figuring out platypus

--- mad-reciept.py
+++ mad-reciept.py
@@ -5,26 +5,47 @@ Created on Thu May 13 14:32:49 2021
 @author: Vatsal Shah
 """
 
-from reportlab.pdfgen import canvas
+#from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, SimpleDocTemplate
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
+#Defining Variables
 pdfName = "Hello.pdf"
+heading = "Making A Difference Foundation"
 width, height = A4
-c = canvas.Canvas(pdfName, pagesize=A4)
-c.translate(20,420)
-c.drawString(40,40,"Making")
-c.setFillColorRGB(48, 52, 0)
+'''
+#Defining Canvas object and Initializing PDF with pdfName
+c = canvas.Canvas(pdfName, pagesize=A4, bottomup = 0)
+
+#Setting the starting cooridnates from (20,20)
+c.translate(20,20)
+
+#Drawing a rectangle.
+c.setFillColorRGB(0.48, 0.52, 0)
 c.rect(0,0, 550, 400, fill = 1)
-c.setFillColor(colors.white)
-c.setStrokeColor(colors.white)
-c.rect(20,20,510,360, fill = 1)
-c.rotate(0)
+
+#
+c.drawString(40,40,heading)
+#c.setFillColor(colors.white)
+#c.setStrokeColor(colors.white)
+#c.rect(20,20,510,360, fill = 1)
+c.rotate(90)
 c.save()
 
-#setFillColorRGB(r, g, b)
+'''
 pdf = SimpleDocTemplate("re.pdf", pagesize = A4)
 styles = getSampleStyleSheet()
+title_style = styles['Heading1']
+title_style.aligment = 2
+title_style.fontSize = 10
+
+par_1 = Paragraph(heading, title_style)
+flowables = [par_1]
+
+pdf.build(flowables)
+
+
+
