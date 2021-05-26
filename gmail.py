2f60165f955d53de1c0f5fd71223a7140cabab66
vatsal-haresh-shah <67568173+vatsal-haresh-shah@users.noreply.github.com>
2021-05-26 14:02:31 +0530

    Email sorted

--- gmail.py
+++ gmail.py
@@ -1,14 +1,23 @@
-from __future__ import print_function
 import os.path
+#Google API Libraries
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+#Email libraries 
+import base64
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+import mimetypes
+import os
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://mail.google.com/']
 
-def main():
+def getService():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
@@ -31,17 +40,66 @@ def main():
             token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
+    return service
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+def sendMessage(service, id, message):
+    try:
+        message = service.users().messages().send(userId = id, body = message).execute()
 
-    if not labels:
-        print('No labels found.')
+        print('Message ID: {}'.format(message['id']))
+
+        return message
+    except Exception as e:
+        print('An error occurred: {}'.format(e))
+        return None
+
+def createMessageAttactment(sender, to, subject, messageText, file,):
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(messageText)
+    message.attach(msg)
+
+    (contentType, encoding) = mimetypes.guess_type(file)
+
+    if contentType is None or encoding is not None:
+        contentType = 'application/octet-stream'
+
+    (mainType, subType) = contentType.split('/',1)
+
+    if mainType == 'text':
+        with open(file, 'rb') as f:
+            msg = MIMEText(f.read().decode("utf-8"), _subtype=subType)
+    elif mainType == 'image':
+        with open(file, 'rb') as f:
+            msg = MIMEImage(f.read(), _subtype = subType)
+    elif mainType == 'application' and subType == 'pdf':
+        with open(file, 'rb') as f:
+            msg = MIMEApplication(f.read(), _subtype = subType)
     else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+        with open(file, 'rb') as f:
+            msg = MIMEBase(mainType, subType)
+            msg.set_payload(f.read())
+
+    fileName = os.path.basename(file)
+    msg.add_header('Content-Dispostion','attachment', filename = fileName)
+    message.attach(msg)
+
+    rawMsg = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
+
+    return{'raw':rawMsg.decode("utf-8")} 
+    
 
 if __name__ == '__main__':
-    main()
+    service = getService()
+    id = 'me'
+    sender = 'reciept.mad@gmail.com'
+    to = 'clever.vatsal@yahoo.com'
+    subject = 'TESTING'
+    body = "Hopefully this will work"
+    file = './Hello.pdf'
+
+    msg = createMessageAttactment(sender, to, subject, body, file)
+    sendMessage(service, id, msg)
--- sheets.py
+++ sheets.py
@@ -1,16 +1,106 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 19 10:17:35 2021
+#PDF Generation libraries
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+#Google Libraries
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from apiclient import errors
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-@author: Vatsal Shah
-"""
+from num2words import num2words
+import pickle
+import os
+#Email Libraries
+import base64
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+import mimetypes
+
 scope = ['https://www.googleapis.com/auth/spreadsheets']
 serviceAccountFile = 'generator-keys.json'
+SCOPES = ['https://mail.google.com/']
+
+def get_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    return service
+
+
+def send_message(service, user_id, message):
+    try:
+        message = service.users().messages().send(userId=user_id, body=message).execute()
+
+        print('Message Id: {}'.format(message['id']))
+
+        return message
+    except Exception as e:
+        print('An error occurred: {}'.format(e))
+        return None
+
+
+def create_message_with_attachment(sender, to, subject, message_text, file,):
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    (content_type, encoding) = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+
+    (main_type, sub_type) = content_type.split('/', 1)
+
+    if main_type == 'text':
+        with open(file, 'rb') as f:
+            msg = MIMEText(f.read().decode('utf-8'), _subtype=sub_type)
+
+    elif main_type == 'image':
+        with open(file, 'rb') as f:
+            msg = MIMEImage(f.read(), _subtype=sub_type)
+        
+    else:
+        with open(file, 'rb') as f:
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(f.read())
+
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    raw_message = base64.urlsafe_b64encode(message.as_string().encode('utf-8'))
+    return {'raw': raw_message.decode('utf-8')}
+
+
+
 
 def sheetData(scope, serviceAccountFile):
-    from googleapiclient.discovery import build
-    from google.oauth2 import service_account
-    from num2words import num2words
     creds = None
     creds = service_account.Credentials.from_service_account_file(serviceAccountFile, scopes=scope)
 
@@ -35,18 +125,16 @@ def sheetData(scope, serviceAccountFile):
             "amountPaid" : row[13],
             "panCardNo" : row[15],
             "bankName" : row[14],
-            "amtWord" : num2words(row[13])
+            "amtWord" : num2words(row[13]),
+            "email" : row[10]
         }
     
-    return data
+    return row
 
-def createReciept(data):
-    from reportlab.pdfgen import canvas
-    from reportlab.lib.pagesizes import A4
-    from reportlab.lib import colors
+def createReciept(data, fileName):
     #Defining Variables
     tR = 'Times-Roman'
-    fileName = 'Reciept.pdf'
+    file = fileName
     docTitle = "Mad-Reciept"
     title = "Making A Difference Foundation"
     subTitle1 = "Registered Under Society Act XXI of 1860, No.: 1353/2014 GBBSD"
@@ -72,7 +160,7 @@ def createReciept(data):
 
 
     #Defining Canvas object and Initializing PDF with pdfName
-    doc = canvas.Canvas("MAD-Reciept " + data["firstName"] + " " + data["lastName"] + ".pdf", pagesize=A4)
+    doc = canvas.Canvas(file, pagesize=A4)
     
     #Give the document a title
     doc.setTitle(docTitle)
@@ -110,7 +198,7 @@ def createReciept(data):
     doc.drawString(110,640, data["srNo"])
     
     doc.drawString(450,640,date)
-    doc.drawString(480,640,currentDate)
+    doc.drawString(480,640, data["date"])
     
     #Name of the Donor
     doc.drawString(40,610, name)
@@ -118,7 +206,7 @@ def createReciept(data):
     doc.drawString(200,610, data["firstName"])
     if data["middleName"] != None:
         doc.drawString(280,610,data["middleName"])
-    doc.drawString(360,610, data["lastName"])
+    doc.drawString(380,610, data["lastName"])
     
     #Amount paid and Pan card No
     doc.setFont(tR, 12)
@@ -149,7 +237,7 @@ def createReciept(data):
     #Amount Box
     doc.rect(60,460,60,30)
     doc.setFont(tR, 16)
-    doc.drawString(70, 470, data["amountPaid"] + "/-")
+    doc.drawString(60, 470, data["amountPaid"] + "/-")
     
     #Apprecition text
     doc.setFont("Courier", 18)
@@ -174,7 +262,19 @@ def createReciept(data):
     #Saving the pdf with the above changes 
     doc.save()
 
+if __name__ == "__main__":
+    data = sheetData(scope,serviceAccountFile)
+    '''
+    user_id = 'me'
+    sender = 'reciept.mad@gmail.com'
+    to = data["email"]
+    sub = "MAD Reciept " + data["firstName"] + " " + data["lastName"]
+    body = 'This is a test, Hopefull it works. Thank you for your donation'
+    file = "MAD-Reciept " + data["firstName"] + " " + data["lastName"] + ".pdf"
+    '''
+    print(data)
 
-data = sheetData(scope,serviceAccountFile)
-
-createReciept(data)
+    #createReciept(data, file)
+    #service = get_service()
+    #msg = create_message_with_attachment(sender, to, sub, body, file)
+    #send_message(service, user_id, msg)
